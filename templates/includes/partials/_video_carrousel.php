d677fee7bee110ecd062c225b2ab4080a17d2586
Souleymane Coulibaly <bina.coulibaly@gmail.com>
2015-09-09 22:23:40 -0400

    moved video markup out of page--front template.

---
+++ templates/includes/partials/_video_carrousel.php
@@ -0,0 +1,77 @@
+<div class="widget kopa-slide-2-widget">
+    <h3 class="widget-title style11">video<span class="ttg"></span></h3>
+    <div class="owl-carousel owl-carousel-7">
+        <div class="item">
+            <article class="entry-item video-post">
+                <div class="entry-thumb">
+                    <a href="#"><img src="http://placehold.it/188x147" alt=""></a>
+                    <a class="thumb-icon" href="#"></a>
+                </div>
+                <div class="entry-content">
+                    <h4 class="entry-title" itemscope="" itemtype="http://schema.org/Event"><a itemprop="name" href="#">USMNT friendly rostter about Dollovan</a></h4>
+                    <footer>
+                        <p class="entry-author">by <a href="#">Michel bellar</a></p>
+                    </footer>
+                </div>
+            </article>
+        </div>
+        <div class="item">
+            <article class="entry-item video-post">
+                <div class="entry-thumb">
+                    <a href="#"><img src="http://placehold.it/188x147" alt=""></a>
+                    <a class="thumb-icon" href="#"></a>
+                </div>
+                <div class="entry-content">
+                    <h4 class="entry-title" itemscope="" itemtype="http://schema.org/Event"><a itemprop="name" href="#">USMNT friendly rostter about Dollovan</a></h4>
+                    <footer>
+                        <p class="entry-author">by <a href="#">Michel bellar</a></p>
+                    </footer>
+                </div>
+            </article>
+        </div>
+        <div class="item">
+            <article class="entry-item video-post">
+                <div class="entry-thumb">
+                    <a href="#"><img src="http://placehold.it/188x147" alt=""></a>
+                    <a class="thumb-icon" href="#"></a>
+                </div>
+                <div class="entry-content">
+                    <h4 class="entry-title" itemscope="" itemtype="http://schema.org/Event"><a itemprop="name" href="#">USMNT friendly rostter about Dollovan</a></h4>
+                    <footer>
+                        <p class="entry-author">by <a href="#">Michel bellar</a></p>
+                    </footer>
+                </div>
+            </article>
+        </div>
+        <div class="item">
+            <article class="entry-item video-post">
+                <div class="entry-thumb">
+                    <a href="#"><img src="http://placehold.it/188x147" alt=""></a>
+                    <a class="thumb-icon" href="#"></a>
+                </div>
+                <div class="entry-content">
+                    <h4 class="entry-title" itemscope="" itemtype="http://schema.org/Event"><a itemprop="name" href="#">USMNT friendly rostter about Dollovan</a></h4>
+                    <footer>
+                        <p class="entry-author">by <a href="#">Michel bellar</a></p>
+                    </footer>
+                </div>
+            </article>
+        </div>
+        <div class="item">
+            <article class="entry-item video-post">
+                <div class="entry-thumb">
+                    <a href="#"><img src="http://placehold.it/188x147" alt=""></a>
+                    <a class="thumb-icon" href="#"></a>
+                </div>
+                <div class="entry-content">
+                    <h4 class="entry-title" itemscope="" itemtype="http://schema.org/Event"><a itemprop="name" href="#">USMNT friendly rostter about Dollovan</a></h4>
+                    <footer>
+                        <p class="entry-author">by <a href="#">Michel bellar</a></p>
+                    </footer>
+                </div>
+            </article>
+        </div>
+    </div>
+    <!-- owl-carousel-7 -->
+</div>
+<!-- widget -->
