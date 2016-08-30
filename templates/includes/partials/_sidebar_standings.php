044166cd3580a185c4c4f290eb555cc7e32d448e
Souleymane Coulibaly <souleymane.coulibaly@aquilent.com>
2016-08-29 22:41:12 -0400

    Updated standings to implement groups

--- templates/includes/partials/_sidebar_standings.php
+++ templates/includes/partials/_sidebar_standings.php
@@ -7,6 +7,7 @@ $standings = footmali_get_standings('2015-2016');
       <div class="widget kopa-charts-widget">
           <h3 class="widget-title style7"><span><?php echo t('standings table'); ?></span></h3>
           <div class="widget-content">
+              <h3 class="widget-title style10">Poule A</h3>
               <header>
                   <div class="t-col"><?php echo t('pos'); ?></div>
                   <div class="t-col width1"><?php echo t('team'); ?></div>
@@ -14,19 +15,44 @@ $standings = footmali_get_standings('2015-2016');
                   <div class="t-col"><?php echo t('pts'); ?></div>
               </header>
               <ul class="clearfix">
-                <?php $index = 1;
-                  foreach ($standings as $points => $row): ?>
-                  <?php
-                    $team = node_load($row->team);
-                    $team_short_name = !empty($team->field_short_name) ? $team->field_short_name[LANGUAGE_NONE][0]['value'] : $team->title;
-                  ?>
-                  <li>
-                      <div class="t-col"><?php echo $index; ?></div>
-                      <div class="t-col width1"><?php echo strlen($team->title) < 15 ? $team->title : $team_short_name; ?></div>
-                      <div class="t-col"><?php echo $row->played; ?></div>
-                      <div class="t-col"><?php echo $row->points; ?></div>
-                  </li>
-                <?php $index++; endforeach; ?>
+                <?php $i1 = 1;
+                    foreach ($standings['pouleA'] as $points => $row): ?>
+                        <?php
+                            $team = node_load($row->team);
+                            $team_short_name = !empty($team->field_short_name) ? $team->field_short_name[LANGUAGE_NONE][0]['value'] : $team->title;
+                            ?>
+                        <li>
+                            <div class="t-col"><?php echo $i1; ?></div>
+                            <div class="t-col width1"><?php echo strlen($team->title) < 15 ? $team->title : $team_short_name; ?></div>
+                            <div class="t-col"><?php echo $row->played; ?></div>
+                            <div class="t-col"><?php echo $row->points; ?></div>
+                        </li>
+                    <?php $i1++; endforeach; ?>
+              </ul>
+              <!-- <a class="kopa-view-all" href="">View all<span class="fa fa-chevron-right"></span></a> -->
+          </div>
+          <div class="widget-content">
+              <h3 class="widget-title style10">Poule B</h3>
+              <header>
+                  <div class="t-col"><?php echo t('pos'); ?></div>
+                  <div class="t-col width1"><?php echo t('team'); ?></div>
+                  <div class="t-col"><?php echo t('p'); ?></div>
+                  <div class="t-col"><?php echo t('pts'); ?></div>
+              </header>
+              <ul class="clearfix">
+                <?php $i2 = 1;
+                    foreach ($standings['pouleB'] as $points => $row): ?>
+                        <?php
+                            $team = node_load($row->team);
+                            $team_short_name = !empty($team->field_short_name) ? $team->field_short_name[LANGUAGE_NONE][0]['value'] : $team->title;
+                        ?>
+                        <li>
+                            <div class="t-col"><?php echo $i2; ?></div>
+                            <div class="t-col width1"><?php echo strlen($team->title) < 15 ? $team->title : $team_short_name; ?></div>
+                            <div class="t-col"><?php echo $row->played; ?></div>
+                            <div class="t-col"><?php echo $row->points; ?></div>
+                        </li>
+                    <?php $i2++; endforeach; ?>
               </ul>
               <!-- <a class="kopa-view-all" href="">View all<span class="fa fa-chevron-right"></span></a> -->
           </div>
